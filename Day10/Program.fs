b5eeafe22a70f31a65f703b60b6fff01287e1dc2
Luke Lauter <gigibyte16@gmail.com>
2023-12-10 15:59:34 -0500

    Day 10 Part 1

--- Day10/Program.fs
+++ Day10/Program.fs
@@ -6,15 +6,22 @@ module PipeTraversal =
     | South
     | East
     | West
+    with
+        member this.rev =
+            match this with
+            | North -> South
+            | South -> North
+            | East -> West
+            | West -> East
 
     let moveDirection currentLoc direction =
         let (x,y) = currentLoc
         match direction with
-        | North -> (x, y + 1)
-        | South -> (x, y - 1)
+        | North -> (x, y - 1)
+        | South -> (x, y + 1)
         | East -> (x + 1, y)
         | West -> (x - 1, y)
-            
+        
     let pipeToDirMap =
         [|
             "|",[|North;South|] |> Set.ofArray
@@ -41,11 +48,53 @@ module PipeTraversal =
             )
             |> Array.ofSeq
         grid,startPos
+    
+    let locateValidStartingDirections startingPos (grid: string array array) =
+        seq {
+            for dir in [|North;South;East;West|] do
+                let (x,y) = moveDirection startingPos dir 
+                match grid[y][x] with
+                | "." -> ()
+                | _ ->
+                    match pipeToDirMap[grid[y][x]] |> Set.contains dir.rev with
+                    | true ->
+                        yield dir
+                    | false -> ()
+        }
+    
+    let rec traverseLoop allSteps currentLoc enterFromDir (grid: string array array) =
+        let (x, y) = currentLoc
+        match grid[y][x] with
+        | "S" -> allSteps
+        | _ as newPipe ->
+            let dir = enterFromDir |> Set.singleton |> Set.difference pipeToDirMap[newPipe] |> Set.minElement
+            let newLoc = moveDirection currentLoc dir
+            traverseLoop (allSteps @ [newLoc]) newLoc dir.rev grid
+
+    let retrieveAllLoopCoords gridAndStartingPos =
+        let (grid, startingPos) = gridAndStartingPos
+        let startingDir = locateValidStartingDirections startingPos grid |> Seq.head
+        let newLoc = moveDirection startingPos startingDir
+        traverseLoop [startingPos;newLoc] newLoc startingDir.rev grid
 
 module Part1 =
     open PipeTraversal
-    let total =
-        collectGridAndStartPos
+    let findFurthestStep gridAndStartingPos =
+        let totalSteps = retrieveAllLoopCoords gridAndStartingPos
+        (totalSteps |> List.length) / 2
 
+    let total lines =
+        collectGridAndStartPos lines
+        |> findFurthestStep
+
+module Part2 =
+    open PipeTraversal
+
+    let total lines =
+        collectGridAndStartPos lines
+        |> fst
 input
-|> outputFileSeqAndResult Part1.total "Part 1"
+|> outputFileResult Part1.total "Part 1"
+
+//input
+//|> outputFileSeq Part2.total "Part 2"
