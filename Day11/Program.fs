25465084f2fffa5017a03a55481669965b8fbd48
Luke Lauter <gigibyte16@gmail.com>
2023-12-11 00:46:44 -0500

    Began Day 11 Part 1

--- Day11/Program.fs
+++ Day11/Program.fs
@@ -1,8 +1,34 @@
 ﻿open Helper
 
-module Part1 =
-    let total lines =
-        lines
+module CosmicMap =
+    let createCosmicMapAndExpansionRowsAndColumns (lines: string seq) =
+        let allGridColsAndRows =
+            (lines |> Seq.mapi(fun i _ -> (0,i)) |> Seq.append (lines |> Seq.head |> Seq.mapi(fun i _ -> (i,0)))) |> Set.ofSeq
+        let mutable gridsWithGalaxies = allGridColsAndRows
+        let grid =
+            lines
+            |> Seq.indexed
+            |> Seq.map(fun (y, line) ->
+                line
+                |> Seq.indexed
+                |> Seq.map(fun (x, char) ->
+                    match string char with
+                    | "#" ->
+                        gridsWithGalaxies <- gridsWithGalaxies.Remove((x,0)).Remove((0,y))
+                    | _ -> ()
+                    string char
+                )
+                |> Array.ofSeq
+            )
+            |> Array.ofSeq
+        grid, gridsWithGalaxies
 
-test
+module Part1 =
+    open CosmicMap
+    let total lines =
+        let (grid, expansionCoords) = createCosmicMapAndExpansionRowsAndColumns lines
+        expansionCoords
+                
+
+input
 |> outputFileSeq Part1.total "Part 1"
