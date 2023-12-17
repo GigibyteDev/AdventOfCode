8a4b9d574805b5c58b4389ee589e4e23bd3d802f
Luke Lauter <gigibyte16@gmail.com>
2023-12-16 19:54:57 -0500

    Day 16 Part 2

--- Day16/Program.fs
+++ Day16/Program.fs
@@ -8,6 +8,16 @@ module MirrorMaze =
         |> Seq.map(fun (line:string) -> line.ToCharArray())
         |> Array.ofSeq
 
+    let gatherAllStartingPositions (grid: char array array) =
+        seq {
+            for i in [0 .. grid.Length - 1] do yield ((0, i),East)
+            for i in [0 .. (grid[0].Length - 1)] do yield ((i,0),South)
+            for i in [0 .. grid[0].Length - 1] do yield ((i, grid.Length - 1),North)
+            for i in [0 .. grid.Length - 1] do yield ((grid[0].Length - 1, i),West)
+        } |> Array.ofSeq
+
+    let topLeftStartOnly grid = gatherAllStartingPositions grid |> Array.head |> Array.singleton
+
     let mirrorConversionMap (enterDir: Direction) mirrorType =
         match mirrorType with
         | '|' -> 
@@ -34,28 +44,58 @@ module MirrorMaze =
             | West -> [|South|]
         | _ -> [|enterDir.rev|]
 
-    let beginLoop grid =
-        let mutable cache: Set<_> = Set.empty
-        let rec traverseMaze coord (grid: char array array) (dir: Direction) (energizedCoords: Set<(int*int)>) =
+    let beginLoop positionGatherFunc grid =
+        let mutable cache': Set<_> = Set.empty
+
+        let rec traverseMaze coord (grid: char array array) (dir: Direction) (energizedCoords: Set<int*int>) =
             match coord with
             | Some coord ->
-                match cache.Contains (coord, dir) with
-                | true -> energizedCoords
+                match cache' .Contains(coord,dir) with
+                | true -> energizedCoords |> Set.union energizedCoords
                 | false ->
-                    cache <- cache.Add(coord, dir)
-                    let (x, y) = coord
-                    mirrorConversionMap dir.rev (grid[y][x])
-                    |> Array.map(fun newDir -> traverseMaze (nextCoordNoLoop grid newDir coord) grid newDir (energizedCoords.Add(coord)))
-                    |> Set.unionMany
+                    cache' <- cache'.Add((coord,dir))
+                    let (x,y) = coord
+                    let newEnergizedCoords =
+                        mirrorConversionMap dir.rev (grid[y][x])
+                        |> Array.map(fun newDir -> 
+                            let newCoord = (nextCoordNoLoop grid newDir coord)
+                            traverseMaze newCoord grid newDir (Set.empty.Add(coord)))
+                        |> Set.unionMany
+                    energizedCoords |> Set.union newEnergizedCoords
             | None -> energizedCoords
 
-        traverseMaze ((0,0) |> Some) grid East Set.empty
+        let startingPositions = 
+            positionGatherFunc grid
+        let allSets = 
+            startingPositions
+            |> Seq.map(fun sp -> 
+                match sp |> fst = (3,0) with
+                | true -> ()
+                | false -> ()
+                cache' <- Set.empty
+                traverseMaze ((sp |> fst) |> Some) grid (sp |> snd) Set.empty
+                )
+        allSets
+        |> Seq.maxBy(fun set -> set |> Seq.length)
+        |> Seq.length
+        
 module Part1 =
     open MirrorMaze
     let total lines =
         lines
         |> convertInputToGrid
-        |> beginLoop
+        |> beginLoop topLeftStartOnly
+
+module Part2 =
+    open MirrorMaze
+    let total lines =
+        lines
+        |> convertInputToGrid
+        |> beginLoop gatherAllStartingPositions
+
 
 input
-|> outputFileSeq Part1.total "Part 1"
+|> outputFileResult Part1.total "Part 1"
+
+input
+|> outputFileResult Part2.total "Part 2"
